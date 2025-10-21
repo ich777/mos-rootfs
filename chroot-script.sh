16e1cd74171acbb68b5e09f96747924d9f0de9a4
Christoph <christophhummer@gmail.com>
2025-10-21 18:14:49 +0200

    Update chroot-script.sh
    - add `dosfstools`

--- chroot-script.sh
+++ chroot-script.sh
@@ -5,7 +5,7 @@ apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get -y install --no-install-recommends --no-install-suggests curl wget nano htop util-linux e2fsprogs dmidecode \
   iputils-ping jq openssh-server parted nvme-cli xfsprogs btrfs-progs ifupdown resolvconf udev git bridge-utils acpid git smbclient cpufrequtils \
   isc-dhcp-client acpi-support-base nginx tree samba xz-utils rsyslog ntp kbd console-data hdparm smartmontools at netcat-openbsd cryptsetup \
-  net-tools sysstat pciutils usbutils nut-server beep inotify-tools libjson-glib-1.0-0 libxml2 libpciaccess0 open-iscsi tgt dos2unix \
+  net-tools sysstat pciutils usbutils nut-server beep inotify-tools libjson-glib-1.0-0 libxml2 libpciaccess0 open-iscsi tgt dos2unix dosfstools \
   iptables libpixman-1-0 libspice-server1 libusbredirparser1 dnsmasq lm-sensors gnutls-bin cron rsync fuse3 nfs-kernel-server nfs-common libpng16-16
 
 DEB_APPS=$(ls -1 /tmp/applications/*.deb)
